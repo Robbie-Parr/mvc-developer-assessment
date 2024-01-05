e9e3fbe5ed06dd23cb2e72f406e9430716976d60
rp1222 <116277379+Robbie-Parr@users.noreply.github.com>
2024-01-05 10:09:19 +0000

    Added 404 when the Blog id is invalid

--- src/Controllers/BlogController.cs
+++ src/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -7,13 +8,15 @@ namespace WebApplication1.Controllers
     public class BlogController : Controller
     {
         [Route("{id}")]
-        public IActionResult Index(int id)
+        public ActionResult Index(int id)
         {
             JSONFunctions JSONfile = new JSONFunctions(
                     "Blog-Posts(Modified).json"
                     );
             BlogPost post = JSONfile.GetData(id);
-
+            if (post.id==0){
+                return NotFound();
+            }
             ViewData["post"] = post;
             return View();
         }
